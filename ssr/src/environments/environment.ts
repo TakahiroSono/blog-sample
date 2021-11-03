99979a7ff71f0eb23e02e82dd8592cf716398707
TakahiroSono <taka.sono889@gmail.com>
2021-11-04 08:58:48 +0900

    add firebase

---
+++ ssr/src/environments/environment.ts
@@ -0,0 +1,24 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
+export const environment = {
+  firebase: {
+    projectId: 'angular-ssr-sample',
+    appId: '1:774207366612:web:00e3a925b033cd7056e241',
+    storageBucket: 'angular-ssr-sample.appspot.com',
+    apiKey: 'AIzaSyCynNocMeIskdpf592_-O_OMIjrzrAKM4c',
+    authDomain: 'angular-ssr-sample.firebaseapp.com',
+    messagingSenderId: '774207366612',
+  },
+  production: false
+};
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
