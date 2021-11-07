a49c1074b1b3e2310054b7534c055637bac5bcb7
TakahiroSono <taka.sono889@gmail.com>
2021-11-07 16:28:05 +0900

    end sample

--- timestamp/src/app/app.component.ts
+++ timestamp/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AngularFirestore } from '@angular/fire/compat/firestore';
+import { Observable, of } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'timestamp';
+  timestamp: Date | undefined = undefined;
+  constructor(private angularFireStore: AngularFirestore){}
+
+  submit() {
+    const data = {
+      createdAt: new Date()
+    }
+    this.angularFireStore.collection('timestamp').add(data).catch(e => {console.log(e)}).then(value => {console.log(value)})
+  }
+
+  getTimestamp() {
+    this.angularFireStore.collection('timestamp').doc('Ltq2jPZPZRmlMNulKKO1').valueChanges().subscribe(value => {
+      this.timestamp = (value as any).createdAt
+      console.log(value)
+    })
+  }
 }
--- timestamp/src/app/app.module.ts
+++ timestamp/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireModule } from '@angular/fire/compat';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,7 +17,9 @@ import { provideFirestore,getFirestore } from '@angular/fire/firestore';
     BrowserModule,
     AppRoutingModule,
     provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideFirestore(() => getFirestore())
+    provideFirestore(() => getFirestore()),
+    AngularFirestoreModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent]
