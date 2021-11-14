75459519c3550f62b92b67b74dbcd9df2537899f
TakahiroSono <taka.sono889@gmail.com>
2021-11-15 08:57:57 +0900

    add firebase

---
+++ v7/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'angularfire-v7',
+    appId: '1:1028327268158:web:452d46677f6c8460c0566f',
+    storageBucket: 'angularfire-v7.appspot.com',
+    apiKey: 'AIzaSyDq4r8NqYVZuS7Qkv--QMYOCW_SmvIF24c',
+    authDomain: 'angularfire-v7.firebaseapp.com',
+    messagingSenderId: '1028327268158',
+  },
+  production: true
+};
