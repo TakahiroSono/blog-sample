395daacf59b2e3da12243d7548128122c74e1e4e
TakahiroSono <taka.sono889@gmail.com>
2021-11-15 08:50:00 +0900

    ssr

---
+++ ssr/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { environment } from '../environments/environment';
+import { provideFunctions,getFunctions } from '@angular/fire/functions';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideFunctions(() => getFunctions())
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ ssr/src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebase: {
+    projectId: 'angular-ssr-sample',
+    appId: '1:774207366612:web:00e3a925b033cd7056e241',
+    storageBucket: 'angular-ssr-sample.appspot.com',
+    apiKey: 'AIzaSyCynNocMeIskdpf592_-O_OMIjrzrAKM4c',
+    authDomain: 'angular-ssr-sample.firebaseapp.com',
+    messagingSenderId: '774207366612',
+  },
+  production: true
+};
