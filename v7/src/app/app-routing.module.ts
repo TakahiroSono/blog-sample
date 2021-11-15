77f55eb846792aea5556efa57a86af7f70a38f43
TakahiroSono <taka.sono889@gmail.com>
2021-11-15 09:02:52 +0900

    initilize

--- v7/src/app/app-routing.module.ts
+++ v7/src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PostcardComponent } from './page/postcard/postcard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', redirectTo: '/postcard', pathMatch: 'full'},
+  {path: 'postcard', component: PostcardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- v7/src/app/app.module.ts
+++ v7/src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { environment } from '../environments/environment';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { PostcardComponent } from './page/postcard/postcard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostcardComponent
   ],
   imports: [
     BrowserModule,
